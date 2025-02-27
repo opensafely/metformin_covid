3c97b22d591884e9982a4f9e076458f80ce5e1d6
Alain Amstutz <131078877+alainamstutz@users.noreply.github.com>
2025-02-27 12:17:22 +0100

    added death as censoring event to all km_estimates

--- analysis/data_process.R
+++ analysis/data_process.R
@@ -777,7 +777,7 @@ data_plots <- data_processed %>%
                 exp_date_sglt2_mono_anytime, exp_bin_sglt2_mono_anytime, exp_date_sulfo_mono_anytime, exp_bin_sulfo_mono_anytime,
                 exp_date_glp1_mono_anytime, exp_bin_glp1_mono_anytime, exp_date_megli_mono_anytime, exp_bin_megli_mono_anytime, 
                 exp_date_agi_mono_anytime, exp_bin_agi_mono_anytime, exp_date_insulin_mono_anytime, exp_bin_insulin_mono_anytime,
-                out_date_severecovid2)
+                out_date_severecovid2, qa_date_of_death)
 
 # Adapt dummy data:
 if (Sys.getenv("OPENSAFELY_BACKEND") %in% c("", "expectations")) {
@@ -793,7 +793,8 @@ if (Sys.getenv("OPENSAFELY_BACKEND") %in% c("", "expectations")) {
     dplyr::filter(elig_date_t2dm < exp_date_glp1_mono_anytime | is.na(exp_date_glp1_mono_anytime)) %>%
     dplyr::filter(elig_date_t2dm < exp_date_megli_mono_anytime | is.na(exp_date_megli_mono_anytime)) %>%
     dplyr::filter(elig_date_t2dm < exp_date_agi_mono_anytime | is.na(exp_date_agi_mono_anytime)) %>%
-    dplyr::filter(elig_date_t2dm < exp_date_insulin_mono_anytime | is.na(exp_date_insulin_mono_anytime))
+    dplyr::filter(elig_date_t2dm < exp_date_insulin_mono_anytime | is.na(exp_date_insulin_mono_anytime)) %>% 
+    dplyr::filter(elig_date_t2dm < qa_date_of_death | is.na(qa_date_of_death))
   
   message("...dummy data successfully adapted")
 }
