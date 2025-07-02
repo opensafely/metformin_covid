21ad4f34b73c07036bdaabb7f33a2df297ff4c28
Alain Amstutz <131078877+alainamstutz@users.noreply.github.com>
2025-07-02 23:54:05 +0100

    Update data_process.R

--- analysis/data_process.R
+++ analysis/data_process.R
@@ -311,7 +311,7 @@ data_processed <- data_processed %>%
 data_processed <- data_processed %>% 
   mutate(
     cox_date_severecovid = pmin(out_date_severecovid_afterlandmark, 
-                                out_date_death_afterlandmark,
+                                out_date_noncoviddeath_afterlandmark,
                                 cens_date_ltfu_afterlandmark,
                                 max_fup_date,
                                 studyend_date,
@@ -320,7 +320,7 @@ data_processed <- data_processed %>%
                                   landmark_date,
                                   units = "days") %>% as.numeric(),
     cox_date_covid = pmin(out_date_covid_afterlandmark, 
-                          out_date_death_afterlandmark,
+                          out_date_noncoviddeath_afterlandmark,
                           cens_date_ltfu_afterlandmark,
                           max_fup_date,
                           studyend_date,
@@ -330,14 +330,13 @@ data_processed <- data_processed %>%
                             units = "days") %>% as.numeric(),
     cox_date_longcovid_virfat = pmin(out_date_longcovid_virfat_afterlandmark, 
                                      out_date_death_afterlandmark,
-                                     out_date_covid_death,
                                      cens_date_ltfu_afterlandmark,
                                      max_fup_date,
                                      studyend_date,
                                      na.rm = TRUE),
     cox_tt_longcovid_virfat = difftime(cox_date_longcovid_virfat,
-                                                     landmark_date,
-                                                     units = "days") %>% as.numeric(),
+                                       landmark_date,
+                                       units = "days") %>% as.numeric(),
     # for cox reusable action: exposure start date for those who start, missing for all others 
     cox_date_metfin_start_within6m = case_when(exp_bin_metfin_mono == TRUE ~ landmark_date, 
                                                TRUE ~ as.Date(NA))
@@ -353,45 +352,50 @@ arrow::write_feather(data_processed_full, here::here("output", "data", "data_pro
 
 # Restrict the dataset for the pipeline onwards, and explore deaths/ltfu before landmark & pandemic ----
 # (1) Explore deaths/ltfu
-# died/ltfu between elig_date_t2dm and landmark -> add to data_processed to filter out
+# died between elig_date_t2dm and landmark -> add to data_processed to filter out
 data_processed <- data_processed %>%
-  mutate(death_ltfu_landmark = (
-    (!is.na(qa_date_of_death) & qa_date_of_death > elig_date_t2dm & qa_date_of_death < landmark_date) |
-      (!is.na(cens_date_dereg) & cens_date_dereg > elig_date_t2dm & cens_date_dereg < landmark_date)
-  ))
+  mutate(death_landmark = case_when(
+    (!is.na(qa_date_of_death) & (qa_date_of_death > elig_date_t2dm) & (qa_date_of_death <= landmark_date)) 
+    # | (!is.na(cens_date_dereg) & (cens_date_dereg > elig_date_t2dm) & (cens_date_dereg <= landmark_date)) 
+    ~ TRUE, 
+    TRUE ~ FALSE
+    )
+  )
 # died/ltfu between landmark and pandemic start -> create and add to separate dataset for descriptive table ones
 data_processed_death_ltfu <- data_processed %>%
   filter(!is.na(exp_bin_treat)) %>% # Filter out those with missing exp_bin_treat, but retain those who died/ltfu before landmark for descriptive purposes
-  mutate(death_ltfu_pandemic = (
-    (!is.na(out_date_death_afterlandmark) & out_date_death_afterlandmark < pandemicstart_date) |
-      (!is.na(cens_date_ltfu_afterlandmark) & cens_date_ltfu_afterlandmark < pandemicstart_date)
-  ))
+  mutate(death_ltfu_pandemic = case_when(
+    (!is.na(out_date_death_afterlandmark) & (out_date_death_afterlandmark < pandemicstart_date)) | # do not include day of start of pandemic, since these will count (and start of pandemic is a bit arbitrary chosen)
+      (!is.na(cens_date_ltfu_afterlandmark) & (cens_date_ltfu_afterlandmark < pandemicstart_date)) ~ TRUE, 
+    TRUE ~ FALSE
+  )
+)
 # overall flag
 data_processed_death_ltfu <- data_processed_death_ltfu %>%
   mutate(
-    death_ltfu_landmark_pandemic = death_ltfu_landmark | death_ltfu_pandemic,
-    death_ltfu_pandemic_without_landmark = case_when(death_ltfu_pandemic & !death_ltfu_landmark ~ TRUE,
-                                                     !death_ltfu_pandemic & !death_ltfu_landmark ~ FALSE,
+    death_ltfu_landmark_pandemic = death_landmark | death_ltfu_pandemic,
+    death_ltfu_pandemic_without_landmark = case_when(death_ltfu_pandemic & !death_landmark ~ TRUE,
+                                                     !death_ltfu_pandemic & !death_landmark ~ FALSE,
                                                      TRUE ~ NA)
     )
 
 # count died/ltfu between elig_date_t2dm and landmark and those fulfilling one of the two final treatment strategies
 count <- data_processed %>%
   summarise(
-    n_death_ltfu_landmark = sum(death_ltfu_landmark, na.rm = TRUE),
+    n_death_landmark = sum(death_landmark, na.rm = TRUE),
     n_exp_bin_treat = sum(is.na(exp_bin_treat), na.rm = TRUE))
     
 # (2) Filter: only keep those fulfilling one of the two final treatment strategies and still alive and in care at landmark 
 data_processed <- data_processed %>%
   filter(
     !is.na(exp_bin_treat),
-    (!death_ltfu_landmark | is.na(death_ltfu_landmark))
+    !death_landmark
     )
 # tibble out incl. midpoint6 rounding (!)
 n_restricted_midpoint6 <- tibble(
   n_before_exclusion_midpoint6 = fn_roundmid_any(nrow(data_processed_death_ltfu), threshold),
   n_exp_bin_treat_midpoint6 = fn_roundmid_any(count$n_exp_bin_treat, threshold),
-  n_death_ltfu_landmark_midpoint6 = fn_roundmid_any(count$n_death_ltfu_landmark, threshold),
+  n_death_landmark_midpoint6 = fn_roundmid_any(count$n_death_landmark, threshold),
   n_after_exclusion_midpoint6 = fn_roundmid_any(nrow(data_processed), threshold))
 
 # (3) Filter main dataset: Only keep necessary variables
