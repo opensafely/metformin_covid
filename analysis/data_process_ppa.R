6722d65f69e7373a7372de4d95c4c8de422a5c98
Alain Amstutz <131078877+alainamstutz@users.noreply.github.com>
2025-07-30 12:15:06 +0100

    Update data_process_ppa.R

--- analysis/data_process_ppa.R
+++ analysis/data_process_ppa.R
@@ -9,27 +9,28 @@
 
 
 # Import libraries and user functions -------------------------------------
-library('arrow')
-library('readr')
-library('here')
-library('dplyr')
-library('tidyr')
-library('purrr')
-library('skimr')
+print('Import libraries and user functions')
+library(arrow)
+library(here)
+library(skimr)
+library(tidyverse)
 source(here::here("analysis", "functions", "fn_extract_data.R"))
 
 
 # Create directories for output -------------------------------------------
+print('Create directories for output')
 fs::dir_create(here::here("output", "data"))
 fs::dir_create(here::here("output", "data_description"))
 
 
 # Import the dataset and pre-process --------------------------------------
+print('Import the dataset and pre-process')
 input_filename <- "dataset_ppa.arrow"
 df <- fn_extract_data(input_filename)
 
 
 # Modify dummy data -------------------------------------------------------
+print('Modify dummy data')
 if (Sys.getenv("OPENSAFELY_BACKEND") %in% c("", "expectations")) {
   message("Running locally, adapt dummy data")
   source("analysis/modify_dummy_data_ppa.R")
@@ -38,6 +39,7 @@ if (Sys.getenv("OPENSAFELY_BACKEND") %in% c("", "expectations")) {
 
 
 # Process the data --------------------------------------------------------
+print('Process the data')
 # Define thresholds for each type of biomarker (see data_process)
 hba1c_min <- 0
 hba1c_max <- 120
@@ -66,11 +68,13 @@ df <- df %>%
 
 
 # Filter main dataset: Only keep necessary variables ----------------------
+print('Filter main dataset: Only keep necessary variables')
 df <- df %>% 
   select(!landmark_date)
 
 
 # Save and inspect full processed dataset ---------------------------------
+print('Save and inspect full processed dataset')
 data_processed_ppa_desc <- skim(df)
 write.csv(data_processed_ppa_desc, file = here::here("output", "data_description", "data_processed_ppa_desc.csv")) # for L4 reviewing only, not for release
 arrow::write_feather(df, here::here("output", "data", "data_processed_ppa.arrow"))
