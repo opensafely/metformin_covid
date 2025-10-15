52e8a4987994cb9750503c545c16ce3b89783df9
Alain Amstutz <131078877+alainamstutz@users.noreply.github.com>
2025-10-15 16:27:04 +0100

    Update fn_fill_forward_incl_baseline.R

--- analysis/functions/fn_fill_forward_incl_baseline.R
+++ analysis/functions/fn_fill_forward_incl_baseline.R
@@ -5,26 +5,26 @@
 ####
 
 fn_fill_forward_incl_baseline <- function(x, baseline_col_name) {
-  # pick() returns a data frame of the current group; select first row
-  base_val <- pick(all_of(baseline_col_name))[[1]][1]  # <<-- take first row only
+  # pick() returns a data frame of the current group (i.e. current covariate within a person) 
+  base_val <- pick(all_of(baseline_col_name))[[1]][1]  # select first row, and convert to vector
   
   first_non_na <- which(!is.na(x))[1]
   
+  # first deal with case where no observed values exist in x
   if (is.na(first_non_na)) {
     if (!is.na(base_val)) {
-      rep(base_val, length(x))
+      rep(base_val, length(x)) # use baseline value (if baseline missing, then all NA)
     } else {
       x
     }
   } else {
+    # in the case where at least one observed value exists in x
     masked <- x
     if (!is.na(base_val)) {
-      masked[1:(first_non_na - 1)] <- base_val
+      masked[1:(first_non_na - 1)] <- base_val # fills all missing values before the first observed value with the baseline.
     } else {
       masked[1:(first_non_na - 1)] <- NA
     }
-    
-    # forward fill
-    tidyr::fill(data.frame(masked), masked, .direction = "down")$masked
+    tidyr::fill(data.frame(masked), masked, .direction = "down")$masked # forward fills missing values in the vector.
   }
 }
